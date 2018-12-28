03a9a1520e97e7bee69f5569c213f9d6d4c598db
prasad4492 <45992516+prasad4492@users.noreply.github.com>
2018-12-28 11:24:03 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -21,3 +21,14 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
+
+var pg = require('pg');
+var connectionString = "postgres://rbpyipkiscqxdo:7333cbd816457ffab9deaf27e761aa5087d8c8df57e9f449ea1dbf4493e71fd1@ec2-54-235-86-226.compute-1.amazonaws.com:5432/d992vcfkvqt2jn";
+
+pg.connect(connectionString, function(err, client, done) {
+   client.query('SELECT * FROM alesforce.account', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   });
+});
